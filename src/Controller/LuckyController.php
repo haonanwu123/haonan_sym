32281966835ecd15da33730858aa5ad38f2d4a9b
haonanwu123 <wuhaonan99@gmail.com>
2023-02-14 10:33:10 +0100

    random-number

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -6,12 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/', name:'home')]
 class LuckyController extends AbstractController
 {
     public function number(): Response
     {
         $number = random_int(0, 100);
         return $this->render('bezoker/number.html.twig',['number'=>$number]);
+    }
+
+    #[Route('lucky/number/{max}', name:'random_number')]
+    public function numbers(int $max):response
+    {
+        $dagen=["maandag", "dinsdag", "woensdag", "donderdag", "vrijdag"];
+        $number = random_int(0,$max);
+        return $this->render('bezoker/random_number.html.twig',['number'=>$number],['days'=>$dagen]);
     }
 
 }
